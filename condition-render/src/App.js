933fd865a48105377d6003a1bb8dfa33cbb84e00
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2021-01-05 14:16:08 -0500

    adding sample content to test conditional rendering

--- condition-render/src/App.js
+++ condition-render/src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+<html>
+<body>
+
+<h1>sample heading</h1>
+
+<p>sample paragraph.</p>
+
+</body>
+</html>
+
     </div>
   );
 }
